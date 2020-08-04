2c702e2aa9bdde218aeaed5b77704c69d40fdf82
Fabian Pranoto <fabian.s.d.p@gmail.com>
2020-08-04 15:20:54 +0700

    added working responsive navbar

--- resources/scripts/responsive-navbar.js
+++ resources/scripts/responsive-navbar.js
@@ -1,8 +1,6 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-}
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
